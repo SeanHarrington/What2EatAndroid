d78eee474861e35f76902cc38b8ccd883716e021
sreinthaler <sreinthaler@mail.csuchico.edu>
2014-04-26 03:35:55 -0700

    Created new icon/logo and background. Changed element colors/opacity to match. Fixed some layout spacing.

--- gen/seanharrington/what2eat/R.java
+++ gen/seanharrington/what2eat/R.java
@@ -22,12 +22,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_about=0x7f020000;
-        public static final int ic_action_event=0x7f020001;
-        public static final int ic_action_ic_action_event=0x7f020002;
-        public static final int ic_action_new_event=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int what2eat_splash=0x7f020005;
+        public static final int bg=0x7f020000;
+        public static final int ic_action_about=0x7f020001;
+        public static final int ic_action_event=0x7f020002;
+        public static final int ic_action_ic_action_event=0x7f020003;
+        public static final int ic_action_new_event=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int main_logo=0x7f020006;
+        public static final int what2eat_splash=0x7f020007;
     }
     public static final class id {
         public static final int about_description=0x7f080003;
